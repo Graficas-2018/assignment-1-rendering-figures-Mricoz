975cd2fb96f9f63082bf07164446b7e55e22700b
Mauricio Rico <mrico005@gmail.com>
2018-08-14 14:49:21 -0500

    Rombo completo

--- Figures.js
+++ Figures.js
@@ -210,7 +210,21 @@ function createTriangle(gl){
 
 function createRhombus(gl)
 {
-    var rhombus = {};
+    var vertexBuffer;
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    var verts = [
+        0.0, 1.2, 0.0,
+        .5, 0.7,  0.0,
+        -.5, 0.7,  0.0,
+        0.0, 0.2, 0.0
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+    var rhombus = {buffer:vertexBuffer, vertSize:3, nVerts:4, primtype:gl.TRIANGLE_STRIP};
+
     return rhombus;
 }
 
